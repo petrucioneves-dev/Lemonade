c8384ce63d0e5913d9105b61ed60975eacb17db0
Petrucio <petrucioneves22@gmail.com>
2025-10-18 18:32:08 -0300

    App Criado

---
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -0,0 +1,130 @@
+package com.example.lemonade
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.lemonade.ui.theme.LemonadeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LemonadeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Lemonade(
+
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Lemonade( modifier: Modifier = Modifier) {
+    var result by remember { mutableIntStateOf(1) }
+    val imagem = when(result){
+        1 -> R.drawable.lemon_tree
+        2 -> R.drawable.lemon_squeeze
+        3 -> R.drawable.lemon_drink
+        else -> R.drawable.lemon_restart
+    }
+    val texto = when(result){
+        1 -> R.string.lemon_tree
+        2 -> R.string.lemon
+        3 -> R.string.glass_of_lemonade
+        else -> R.string.empty_glass
+    }
+
+    Column (
+        modifier = modifier
+    ){
+        Column(
+            modifier = Modifier.background(Color.Yellow).fillMaxHeight(0.1f).fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+
+
+            ) {
+            Text(
+                text = "Lemonade",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+                )
+        }
+        Column (
+            modifier = Modifier.fillMaxHeight().fillMaxWidth().background(Color.White),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(imagem),
+                modifier = Modifier,
+                contentDescription = R.string.lemon_tree.toString()
+            )
+            Text(
+                text = stringResource(texto),
+                modifier= modifier.clickable(
+                    onClick = {
+                        if(result == 4) {
+                            result = 1
+                        } else {
+                            result += 1
+                        }
+                    }
+                ),
+                fontSize = 16.sp,
+                color = Color.Black
+            )
+
+        }
+    }
+
+
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Lemonade(
+
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
